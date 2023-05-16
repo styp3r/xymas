c0ed72b3d9a7f14b56cb21eb23fb587ab15cd9c8
styp3r <stypertypes@gmail.com>
2023-05-16 11:52:54 +0530

    Connects to xymas supabase table and retreives table data (date and sale figure)

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,59 +2,61 @@ import React, { useState } from 'react';
 import Option1 from './Option1'
 import Option2 from './Option2'
 import Option3 from './Option3'
+import Logo from '../images/xymasLogo.png'
 
 const Home = () => {
     const [selectedOption, setSelectedOption] = useState('option1Btn');
-  
+
     const handleOptionClick = (option) => {
-      setSelectedOption(option);
+        setSelectedOption(option);
 
-      document.getElementById("option1Btn").style.backgroundColor = '#ffffff';
-      document.getElementById("option1Btn").style.color = '#a6a6a6';
-      document.getElementById("option2Btn").style.backgroundColor = '#ffffff';
-      document.getElementById("option2Btn").style.color = '#a6a6a6';
-      document.getElementById("option3Btn").style.backgroundColor = '#ffffff';
-      document.getElementById("option3Btn").style.color = '#a6a6a6';
+        document.getElementById("option1Btn").style.backgroundColor = '#ffffff';
+        document.getElementById("option1Btn").style.color = '#8b8b8b';
+        document.getElementById("option2Btn").style.backgroundColor = '#ffffff';
+        document.getElementById("option2Btn").style.color = '#8b8b8b';
+        document.getElementById("option3Btn").style.backgroundColor = '#ffffff';
+        document.getElementById("option3Btn").style.color = '#8b8b8b';
 
-      document.getElementById(option).style.backgroundColor = '#755bb4';
-      document.getElementById(option).style.color = '#ffffff';
+        document.getElementById(option).style.backgroundColor = '#755bb4';
+        document.getElementById(option).style.color = '#ffffff';
     };
-  
+
     let componentToDisplay = null;
-  
+
     switch (selectedOption) {
-      case 'option1Btn':
-        componentToDisplay = <Option1 />;
-        break;
-      case 'option2Btn':
-        componentToDisplay = <Option2 />;
-        break;
-      case 'option3Btn':
-        componentToDisplay = <Option3 />;
-        break;
-      default:
-        componentToDisplay = null;
-        break;
+        case 'option1Btn':
+            componentToDisplay = <Option1 />;
+            break;
+        case 'option2Btn':
+            componentToDisplay = <Option2 />;
+            break;
+        case 'option3Btn':
+            componentToDisplay = <Option3 />;
+            break;
+        default:
+            componentToDisplay = null;
+            break;
     }
-  
+
     return (
-      <div className="dashboard-container">
-        <div className="side-panel" style={{ backgroundColor: '#ffffff' }}>
-          <div id = "option1Btn" className="option" onClick={() => handleOptionClick('option1Btn')}>
-            Option 1
-          </div>
-          <div id = "option2Btn" className="option" onClick={() => handleOptionClick('option2Btn')}>
-            Option 2
-          </div>
-          <div id = "option3Btn" className="option" onClick={() => handleOptionClick('option3Btn')}>
-            Option 3
-          </div>
+        <div className="dashboard-container">
+            <div className="side-panel" style={{ backgroundColor: '#ffffff' }}>
+                <img id = "logo" src = {Logo} ></img>
+                <div id="option1Btn" className="option" onClick={() => handleOptionClick('option1Btn')}>
+                    O Option 1
+                </div>
+                <div id="option2Btn" className="option" onClick={() => handleOptionClick('option2Btn')}>
+                    O Option 2
+                </div>
+                <div id="option3Btn" className="option" onClick={() => handleOptionClick('option3Btn')}>
+                    O Option 3
+                </div>
+            </div>
+            <div className="content">
+                {componentToDisplay}
+            </div>
         </div>
-        <div className="content">
-          {componentToDisplay}
-        </div>
-      </div>
     );
-  };
-  
-  export default Home;
+};
+
+export default Home;
--- src/components/Option1.js
+++ src/components/Option1.js
@@ -1,7 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient('https://lgydkxizvydkathymrad.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxneWRreGl6dnlka2F0aHltcmFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQyMTU5ODAsImV4cCI6MTk5OTc5MTk4MH0.nj-_Ft-7vGi22FnKEEfCh8eH5Cd3KimkjyOxagZsvHg');
+
 const Option1 = () => {
-    return (
-        <p>Option 1 Page</p>
-    );
-}
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchInitialData();
+  }, []);
+
+  const fetchInitialData = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('dailyUPISale')
+        .select();
+      if (error) {
+        throw error;
+      }
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const insertRecord = async () => {
+    try {
+      const { data, error } = await supabase
+        .from('your_table_name')
+        .insert([{ column_name: 'value' }]);
+      if (error) {
+        throw error;
+      }
+      setData((prevData) => [...prevData, ...data]);
+      console.log('Record inserted successfully.');
+    } catch (error) {
+      console.error('Error inserting record:', error);
+    }
+  };
+
+  const updateRecord = async (id, newData) => {
+    try {
+      const { data, error } = await supabase
+        .from('your_table_name')
+        .update(newData)
+        .eq('id', id);
+      if (error) {
+        throw error;
+      }
+      setData((prevData) =>
+        prevData.map((item) => (item.id === id ? { ...item, ...newData } : item))
+      );
+      console.log('Record updated successfully.');
+    } catch (error) {
+      console.error('Error updating record:', error);
+    }
+  };
+
+  const deleteRecord = async (id) => {
+    try {
+      const { error } = await supabase
+        .from('your_table_name')
+        .delete()
+        .eq('id', id);
+      if (error) {
+        throw error;
+      }
+      setData((prevData) => prevData.filter((item) => item.id !== id));
+      console.log('Record deleted successfully.');
+    } catch (error) {
+      console.error('Error deleting record:', error);
+    }
+  };
+
+  return (
+    <div>
+      {/* Your Option1 content */}
+      {data.map((sale) => (
+          <p key={sale.id}>{sale.created_at} - Rs.{sale.upi_sale_figure}</p>
+        ))} 
+    </div>
+  );
+};
 
 export default Option1;
